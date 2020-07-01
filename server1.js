df2249372a7f92dc0572a1b18e197bc3bdc5c365
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-06-30 21:07:24 -0500

    leveraged aactivity 5 code

--- server1.js
+++ server1.js
@@ -5,59 +5,78 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing
+// Will needs these lines of code in the future - allow me to recieve riched JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Data
 // ===========================================================
-var characters = [{
-	routeName: "yoda",
-	name: "Yoda",
-	role: "Jedi Master",
-	age: 900,
-	forcePoints: 2000
-  }, {
-	routeName: "darthmaul",
-	name: "Darth Maul",
-	role: "Sith Lord",
-	age: 200,
-	forcePoints: 1200
-  }, {
-	routeName: "obiwankenobi",
-	name: "Obi Wan Kenobi",
-	role: "Jedi Master",
-	age: 55,
-	forcePoints: 1350
-  }];
-  
-  // Routes
-  // ===========================================================
-  app.get("/", function(req, res) {
+var characters = [
+	{
+		routeName: "yoda",
+		name: "Yoda",
+		role: "Jedi Master",
+		age: 900,
+		forcePoints: 2000,
+	},
+	{
+		routeName: "darthmaul",
+		name: "Darth Maul",
+		role: "Sith Lord",
+		age: 200,
+		forcePoints: 1200,
+	},
+	{
+		routeName: "obiwankenobi",
+		name: "Obi Wan Kenobi",
+		role: "Jedi Master",
+		age: 55,
+		forcePoints: 1350,
+	},
+];
+
+// Routes
+// ===========================================================
+app.get("/", function (req, res) {
 	res.send("Welcome to the Star Wars Page!");
-  });
-  
-  // Displays all characters
-  app.get("/api/characters", function(req, res) {
+});
+
+// Displays all characters
+app.get("/api/characters", function (req, res) {
 	return res.json(characters);
-  });
-  
-  // Displays a single character, or shows "No character found"
-  app.get("/api/characters/:character", function(req, res) {
+});
+
+// Displays a single character, or shows "No character found"
+app.get("/api/characters/:character", function (req, res) {
 	// Grab the selected parameter
 	var chosen = req.params.character;
 	console.log(chosen);
-  
+
 	// Filter to show only the selected character
 	for (var i = 0; i < characters.length; i++) {
-	  if (chosen === characters[i].routeName) {
-		return res.json(characters[i]);
-	  }
+		if (chosen === characters[i].routeName) {
+			return res.json(characters[i]);
+		}
 	}
-  
+
 	// Otherwise display "No character found"
 	return res.send("No character found");
-  });
-  
-  // Listener
-  // ===========================================================
-  app.listen(PORT, function() {
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/characters", function (req, res) {
+	var newCharacter = req.body;
+
+	console.log(newCharacter);
+
+	characters.push(newCharacter);
+
+	res.json(newCharacter);
+});
+
+// Listener
+// ===========================================================
+app.listen(PORT, function () {
 	console.log("App listening on PORT " + PORT);
-  });
-  
+});
