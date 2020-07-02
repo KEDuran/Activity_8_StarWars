b0a9b0a8d704f686cd3080c1cbc9bc57941dad7e
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-01 20:49:40 -0500

    modified post code back to originally provided sample

--- server1.js
+++ server1.js
@@ -74,7 +74,7 @@ app.get("/api/characters/:character", function (req, res) {
 app.post("/api/characters", function (req, res) {
 	// req.body hosts is equal to the JSON post sent from the user
 	// This works because of our body parsing middleware
-	var newCharacter = JSON.parse(req.body);
+	var newCharacter = req.body;
 
 	// Using a RegEx Pattern to remove spaces from newCharacter
 	// You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
