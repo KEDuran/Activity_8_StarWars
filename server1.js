53d6b2bdda2f364141586349f8659c8c01b56990
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-01 20:28:46 -0500

    added files from activity 14

--- server1.js
+++ server1.js
@@ -43,8 +43,12 @@ var characters = [
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
 	// res.send("Welcome to the Star Wars Page!")
-	res.sendFile(path.join(__dirname, "view.html"));
+	res.sendFile(path.join(__dirname, "view.html"))
 });
+
+app.get("/add", function(req, res) {
+	res.sendFile(path.join(__dirname, "add.html"));
+  });
 
 // Displays all characters
 app.get("/api/characters", function (req, res) {
