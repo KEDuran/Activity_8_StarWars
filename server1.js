efb557dedf29dbd778b7e4bad0d18840dc3e374d
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-01 20:38:52 -0500

    copied changes from activity 14

--- server1.js
+++ server1.js
@@ -43,12 +43,12 @@ var characters = [
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
 	// res.send("Welcome to the Star Wars Page!")
-	res.sendFile(path.join(__dirname, "view.html"))
+	res.sendFile(path.join(__dirname, "view.html"));
 });
 
-app.get("/add", function(req, res) {
+app.get("/add", function (req, res) {
 	res.sendFile(path.join(__dirname, "add.html"));
-  });
+});
 
 // Displays all characters
 app.get("/api/characters", function (req, res) {
@@ -74,15 +74,17 @@ app.get("/api/characters/:character", function (req, res) {
 app.post("/api/characters", function (req, res) {
 	// req.body hosts is equal to the JSON post sent from the user
 	// This works because of our body parsing middleware
-	var newcharacter = req.body;
+	var newCharacter = req.body;
 
-	console.log(newcharacter);
+	// Using a RegEx Pattern to remove spaces from newCharacter
+	// You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+	newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
 
-	// We then add the json the user sent to the character array
-	characters.push(newcharacter);
+	console.log(newCharacter);
 
-	// We then display the JSON to the users
-	res.json(newcharacter);
+	characters.push(newCharacter);
+
+	res.json(newCharacter);
 });
 
 // Starts the server to begin listening
